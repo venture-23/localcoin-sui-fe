729ca4c6ff29314e7875a29c00b1313ca3cd286c
sujanxcel <117258799+sujanxcel@users.noreply.github.com>
2023-11-29 20:02:46 +0545

    new

--- app/merchant/page.tsx
+++ app/merchant/page.tsx
@@ -1,3 +1,18 @@
+import Header from 'components/layout/header';
+import Link from 'next/link';
+
 export default function Merchant() {
-  return <>This is Merchant page.</>;
+  return (
+    <>
+      <Header className="h-[120px]">
+        <div className="flex items-center">
+          <Link href="/">{'<- '}</Link>
+          <p className="flex-1  text-center text-2xl font-semibold">LocalCoin</p>
+        </div>
+      </Header>
+      <section>
+        <div></div>
+      </section>
+    </>
+  );
 }
--- app/recipient/page.tsx
+++ app/recipient/page.tsx
@@ -1,3 +1,44 @@
+import Card from 'components/card';
+import Header from 'components/layout/header';
+import Link from 'next/link';
+
 export default function Recipient() {
-  return <>This is Recipient page.</>;
+  return (
+    <>
+      <Header className="h-[120px]">
+        <div className="flex items-center">
+          <Link href="/">{'<- '}</Link>
+          <p className="flex-1  text-center text-2xl font-semibold">Recipient profile</p>
+        </div>
+      </Header>
+      <section className="my-6">
+        <div className="container mx-auto">
+          <div className="mb-6">
+            <h1 className="mb-2 text-2xl font-bold">Signup</h1>
+            <p className="max-w-sm text-gray-700">
+              LocalCoin is just around the corner. Choose an option below to get started .
+            </p>
+          </div>
+
+          <div className="grid grid-cols-2 gap-4">
+            <Card
+              cardContainerClass="justify-center min-h-[134px]"
+              title="Merchant"
+              link="/merchant"
+            />
+            <Card
+              cardContainerClass="justify-center min-h-[134px]"
+              title="Recipient"
+              link="/recipient"
+            />
+            <Card
+              cardContainerClass="justify-center min-h-[134px]"
+              title="Campaign Creator"
+              link="/campaigns"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
