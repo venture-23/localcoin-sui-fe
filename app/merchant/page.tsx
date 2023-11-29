985e6b95d3721d1eaafff8dede89ddd17bca9055
sujanxcel <117258799+sujanxcel@users.noreply.github.com>
2023-11-29 20:40:02 +0545

    pull new

--- app/merchant/page.tsx
+++ app/merchant/page.tsx
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-import Header from 'components/layout/header';
-import Link from 'next/link';
-
-export default function Merchant() {
-  return (
-    <>
-      <Header className="h-[120px]">
-        <div className="flex items-center">
-          <Link href="/">{'<- '}</Link>
-          <p className="flex-1  text-center text-2xl font-semibold">LocalCoin</p>
-        </div>
-      </Header>
-      <section>
-        <div></div>
-      </section>
-=======
 import MerchantPage from 'container/merchantpage';
 export default function Recipient() {
   return (
     <>
       <MerchantPage />
->>>>>>> 50925df1402f2a6f0ef2523818691352a689cb31
     </>
   );
 }
--- container/merchantpage/index.tsx
+++ container/merchantpage/index.tsx
@@ -10,32 +10,38 @@ const RecipientPage = () => {
       <Header className="h-[120px]">
         <div className="flex items-center">
           {/* <Link href="/">{'<- '}</Link> */}
-          <p className="flex-1  text-center text-2xl font-semibold">LocalCoin</p>
+          <p className="flex-1  text-center text-2xl font-semibold">Merchant Profile</p>
         </div>
       </Header>
       <section className="my-6">
         <div className="container mx-auto">
-          <div className="mb-6">
-            <h1 className="mb-2 text-2xl font-bold">Merchant Profile</h1>
+          <div className="mb-6 flex justify-between">
             <h2 className="mb-2 text-2xl font-bold">Your Tokens</h2>
           </div>
 
-          <div className="grid grid-cols-1 gap-1">
-            <TokenCard cardContainerClass="min-h-[50px]" tokenDetails={tokenDetails} />
-            <TokenCard cardContainerClass="min-h-[50px]" tokenDetails={tokenDetails} />
-            <TokenCard cardContainerClass="min-h-[50px]" tokenDetails={tokenDetails} />
+          <div className="mb-6 grid grid-cols-1 gap-3">
+            <TokenCard
+              cardContainerClass="min-h-[50px] justify-between"
+              tokenDetails={tokenDetails}
+            />
+            <TokenCard
+              cardContainerClass="min-h-[50px] justify-between"
+              tokenDetails={tokenDetails}
+            />
+            <TokenCard
+              cardContainerClass="min-h-[50px] justify-between"
+              tokenDetails={tokenDetails}
+            />
           </div>
-        </div>
-      </section>
-      <section className="my-6">
-        <div className="container mx-auto">
-          <div className="mb-6">
+
+          <div className="">
             <h2 className="mb-2 text-2xl font-bold">Settlements</h2>
           </div>
 
-          <div className="grid grid-cols-1 gap-1">
-            <TokenCard cardContainerClass="min-h-[50px]" tokenDetails={settlementDetails} />
-            <TokenCard cardContainerClass="min-h-[50px]" tokenDetails={settlementDetails} />
+          <div className="grid grid-cols-1 gap-3">
+            <button className="h-[52px] rounded-md bg-blue-500 py-4 text-white">
+              Request for settlement
+            </button>
           </div>
         </div>
       </section>
