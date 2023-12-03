e4cb3b6e1844af16a4dbe2cf985eb6ef9d50ef7d
naraharisijapati <narahari.sijapati@bitsbeat.com>
2023-12-03 23:33:18 +0545

    minor fixes done

--- app/recipient/confirmation/page.tsx
+++ app/recipient/confirmation/page.tsx
@@ -32,7 +32,7 @@ export default function confirmation() {
             </div>
             <div>
               <p className="font-bold">Token </p>
-              <p>50$</p>
+              <p>$ 50</p>
             </div>
           </div>
           <div className="mt-6">
