976b1825a36d09b99f38dc06baf88b9c944c6c37
sujanxcel <117258799+sujanxcel@users.noreply.github.com>
2023-12-07 22:21:49 +0545

    new push

--- container/recipientpage/index.tsx
+++ container/recipientpage/index.tsx
@@ -3,6 +3,12 @@ import Header from 'components/layout/header';
 import TokenCard from 'components/tokencard';
 import Link from 'next/link';
 import FundedCampagins from './funded-campagns';
+import Card from 'components/card';
+import {
+  BuildingStorefrontIcon,
+  GlobeEuropeAfricaIcon,
+  UserCircleIcon
+} from '@heroicons/react/24/outline';
 
 const RecipientPage = () => {
   const tokenDetails: any = { name: 'Token1', value: '10.11' };
@@ -14,39 +20,42 @@ const RecipientPage = () => {
   };
   return (
     <>
-      <Header className="h-[120px]">
+      {/* <Header className="h-[120px]">
         <div className="flex items-center">
-          {/* <Link href="/">{'<- '}</Link> */}
-          <p className="flex-1  text-center text-2xl font-semibold">Recipient profile</p>
+          <p className="flex-1 text-2xl font-semibold text-center">Recipient profile</p>
         </div>
-      </Header>
-      <section className="my-6 ">
+      </Header> */}
+      {/* <Link href="/">{'<- '}</Link> */}
+      <section className="">
         <div className="container mx-auto">
-          <div className=" mb-6 border-b pb-6  ">
-            <div className="mb-4 flex items-center justify-between">
-              <h2 className="mb-2 text-2xl font-bold">Your Tokens</h2>
-              <Link href="/recipient/tokens">
-                <p className="mb-2"> View All</p>
-              </Link>
-            </div>
-
-            <div className=" grid gap-3">
-              <TokenCard
-                cardContainerClass="min-h-[50px] justify-between"
-                tokenDetails={tokenDetails}
-              />
-              <TokenCard
-                cardContainerClass="min-h-[50px] justify-between"
-                tokenDetails={tokenDetails}
-              />
-              <TokenCard
-                cardContainerClass="min-h-[50px] justify-between"
-                tokenDetails={tokenDetails}
-              />
+          <div className="mb-6 flex items-center justify-between ">
+            <p className="text-heading">Recipient Profile</p>
+            <div className="h-12 w-12 rounded-full bg-gray-600"></div>
+          </div>
+          <div className="mb-6 border-b pb-6 ">
+            <div className="grid gap-3 ">
+              <TokenCard cardContainerClass=" justify-between" tokenDetails={tokenDetails} />
+              <div className="grid gap-5">
+                <Card
+                  title="Your Tokens"
+                  link="/signup/merchant"
+                  iconName={<BuildingStorefrontIcon className="text-primary h-8 w-8" />}
+                />
+                <Card
+                  title="Ongoing Campaigns"
+                  link="/signup/recipient"
+                  iconName={<UserCircleIcon className="text-primary h-8 w-8" />}
+                />
+                <Card
+                  title="Funded Campaigns "
+                  link="/signup/campaign"
+                  iconName={<GlobeEuropeAfricaIcon className="text-primary h-8 w-8" />}
+                />
+              </div>
             </div>
           </div>
 
-          <div className=" mb-6 border-b pb-6  ">
+          <div className="mb-6 border-b pb-6 ">
             <div className="mb-4 flex justify-between">
               <h2 className="mb-2 text-2xl font-bold">Ongoing Campaigns</h2>
               <Link href="/recipient/campaigns">
@@ -70,7 +79,7 @@ const RecipientPage = () => {
             </div>
           </div>
 
-          <div className=" mb-6 border-b pb-6  ">
+          <div className="mb-6 border-b pb-6 ">
             <div className="mb-4 flex justify-between">
               <h2 className="mb-2 text-2xl font-bold">Funded Campaigns</h2>
               <Link href="/recipient/campaigns">
