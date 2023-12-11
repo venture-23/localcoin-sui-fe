c1bf3d3ee0f523ef6b9bb144fbbe50d6af08ba06
sujanxcel <117258799+sujanxcel@users.noreply.github.com>
2023-12-11 23:16:18 +0545

    new

--- app/recipient/scan-pay/page.tsx
+++ app/recipient/scan-pay/page.tsx
@@ -1,6 +1,8 @@
 'use client';
-import { QrCodeIcon } from '@heroicons/react/24/outline';
+import { ArrowDownOnSquareStackIcon, QrCodeIcon, ShareIcon } from '@heroicons/react/24/outline';
+import Button from 'components/botton';
 import Drawer from 'components/drawer';
+import PopupBox from 'components/popover';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import QRCode from 'qrcode';
@@ -80,6 +82,7 @@ export default function ScanPayMerchant() {
       {/* </Header> */}
       <div ref={cameraRef}>
         <QrReader
+          className="hello"
           onResult={(result, error) => {
             if (!!result) {
               handleScanWebCam(result?.text);
@@ -91,15 +94,24 @@ export default function ScanPayMerchant() {
           }}
           scanDelay={500}
           constraints={{ facingMode: 'rear' }}
-          videoContainerStyle={{ width: '100%', height: '100%' }}
+          containerStyle={{ paddingTop: '0' }}
+          videoContainerStyle={{ width: '100%', height: '100vh', paddingTop: '0' }}
+          videoStyle={{
+            height: '100%',
+            width: '100%'
+          }}
         />
+
+        <span className="try-css bg-tranparent absolute inset-0 m-auto h-[200px] w-1/2 rounded-md border-2 border-white "></span>
+
+        {/* <span className="absolute inset-0 w-full h-full font-bold bg-black/5"></span> */}
       </div>
       {/* <h3>Scanned By WebCam Code: {scanResultWebCam}</h3> */}
       {/* {imageUrl ? <img src={imageUrl} alt="img" style={{ width: '100%' }} /> : null} */}
 
       {imageUrl ? (
         <>
-          <div className="fixed bottom-7 right-7 " onClick={() => setOpen(true)}>
+          <div className="fixed bottom-7 right-7 " onClick={() => setIsOpenPopup(true)}>
             <Link
               // href={asPath.includes('recipient') ? '/recipient/scan-pay' : '/merchant/scan-pay'}
               href=""
@@ -109,10 +121,28 @@ export default function ScanPayMerchant() {
               <span className="text-base font-semibold text-white">Share QR</span>
             </Link>
           </div>
+          <PopupBox
+            PopupTitle="Share QR Code"
+            setIsOpenPopup={setIsOpenPopup}
+            isOpenPopup={isOpenPopup}
+            imageUrl={imageUrl}
+          >
+            <a href={imageUrl} download className="w-full">
+              <Button
+                buttonIcon={<ArrowDownOnSquareStackIcon width={24} height={24} />}
+                text="Save image"
+              />
+            </a>
+            <Button
+              text="Share"
+              buttonType="secondary"
+              buttonIcon={<ShareIcon width={24} height={24} />}
+            />
+          </PopupBox>
 
-          <Drawer open={open} setOpen={setOpen} panelTitle="Share your QR Code">
+          {/* <Drawer open={open} setOpen={setOpen} panelTitle="Share your QR Code">
             <img src={imageUrl} alt="img" style={{ width: '100%' }} />
-          </Drawer>
+          </Drawer> */}
           {/* <BottomSheet divClass="!h-[100vh]">
             <div className="container mx-auto">
               <p className="pt-4 text-center "> Swipe or click to share QR code </p>
