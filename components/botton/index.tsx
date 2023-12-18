592f09be601ac895659602c70cbf5fde646226aa
naraharisijapati <narahari.sijapati@bitsbeat.com>
2023-12-18 19:17:50 +0545

    scan pay issue for recipient resolved

--- components/botton/index.tsx
+++ components/botton/index.tsx
@@ -43,7 +43,7 @@ const Button: React.FC<ButtonProps> = ({
         >
           <div>{buttonIcon}</div>
           <p className="m-0">{text}</p>
-          <svg
+          {/* <svg
             className="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -62,7 +62,7 @@ const Button: React.FC<ButtonProps> = ({
               fill="currentColor"
               d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
             ></path>
-          </svg>
+          </svg> */}
         </button>
       </Link>
     </>
