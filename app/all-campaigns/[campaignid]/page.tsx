5cc692dfb1091ea1a32f18f8d4ad929813755c06
Nitesh Karna <henrynewling@gmail.com>
2024-05-24 13:30:40 +0545

    Fix: minor typo

--- app/all-campaigns/[campaignid]/page.tsx
+++ app/all-campaigns/[campaignid]/page.tsx
@@ -3,7 +3,6 @@ import CampaignDetails from 'container/Campaign/CampaignDetails';
 export default function campaignDetails({ params }: { params: { campaignid: string } }) {
   return (
     <>
-      {/* <h1>Fetch Details of the campaign {params.campaignid}</h1> */}
       <CampaignDetails back="/all-campaigns" campaignId={params.campaignid} />
     </>
   );
